8a0c2503b38b76c37fed21525944769ed018a789
Anir Jung Thapa <134839446+Aneer-Thapa1@users.noreply.github.com>
2024-11-13 20:34:21 +0545

    todo frontend

---
+++ todo-list/App.js
@@ -0,0 +1,38 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Task from "./components/Task";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      {/* Today's Task */}
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today's task</Text>
+
+        <View style={styles.item}>
+          {/* This is where the tasks will go! */}
+          <Task text={"task1"} />
+          <Task text={"task2"} />
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#E8EAED",
+  },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  item: {
+    marginTop: 30,
+  },
+});
